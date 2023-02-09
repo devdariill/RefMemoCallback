6463d9d569def03044feb992ff8e807cfc52ce9f
nATOr <gowluonade@gmail.com>
2023-02-09 05:19:00 -0500

    useRef cache

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -1,12 +1,25 @@
-import responseMovies from '../mocks/with-results.json'
-// import noResponse from '../mocks/no-results.json'
-export function useMovies() {
-  const movies = responseMovies.Search
-  const mappedMovies = movies.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
-  return { movies: mappedMovies }
+// import withResponse from '../mocks/with-results.json'
+import { useState, useRef } from 'react'
+import { searchMovies } from '../services/movies'
+export function useMovies({ query }) {
+  const [movies, setMovies] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const previousSearch = useRef(query)
+  const getMovies = async () => {
+    if (query === previousSearch.current) return
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = query
+      const newMovies = await searchMovies({ query })
+      setMovies(newMovies)
+    } catch (error) {
+      setError(error.message)
+    } finally {
+      // se ejecuta al final de cualquier caso (try, catch)
+      setLoading(false)
+    }
+  }
+  return { movies, getMovies, loading, error }
 }
