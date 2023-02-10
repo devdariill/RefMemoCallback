6e3458222e87973f6bc477d98d4625399c80ab85
nATOr <gowluonade@gmail.com>
2023-02-10 07:10:05 -0500

    render by query

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -8,23 +8,25 @@ export function useMovies({ query, sort }) {
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
   const previousSearch = useRef(query)
-  const getMovies = async () => {
-    if (query === previousSearch.current) return
-    // if (query === previousSearch) return
-    try {
-      setLoading(true)
-      setError(null)
-      previousSearch.current = query
-      // previousSearch = query
-      const newMovies = await searchMovies({ query })
-      setMovies(newMovies)
-    } catch (error) {
-      setError(error.message)
-    } finally {
-      // se ejecuta al final de cualquier caso (try, catch)
-      setLoading(false)
+  const getMovies = useMemo(() => {
+    return async () => {
+      if (query === previousSearch.current) return
+      // if (query === previousSearch) return
+      try {
+        setLoading(true)
+        setError(null)
+        previousSearch.current = query
+        // previousSearch = query
+        const newMovies = await searchMovies({ query })
+        setMovies(newMovies)
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        // se ejecuta al final de cualquier caso (try, catch)
+        setLoading(false)
+      }
     }
-  }
+  }, [query])
   // const sortMovies = sort
   //   ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
   //   : movies
